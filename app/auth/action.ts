57664ce572f32f78c6b3daf3c85db9aa9139a0ee
lyrian12 <lyriannono@gmail.com>
2026-03-03 00:20:08 +0100

    j ai ajouter la confirmation des mots de passe et regler tout les soucis d'authentification

---
+++ app/auth/action.ts
@@ -0,0 +1,26 @@
+'use server'
+
+import { createClient } from '@/lib/supabase/server'
+import { headers } from 'next/headers'
+import { redirect } from 'next/navigation'
+
+export async function signUp(formData: FormData) {
+  const origin = (await headers()).get('origin')
+  const email = formData.get('email') as string
+  const password = formData.get('password') as string
+  const supabase = await createClient()
+
+  const { error } = await supabase.auth.signUp({
+    email,
+    password,
+    options: {
+      emailRedirectTo: `${origin}/auth/callback`,
+    },
+  })
+
+  if (error) {
+    return redirect('/?message=Erreur d’inscription')
+  }
+
+  return redirect('/?message=Vérifiez votre boîte mail pour confirmer votre compte Afriland')
+}
